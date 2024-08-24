f7d62e4455352425955dd999f8f7120c641feec5
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-24 15:13:00 +0300

    done

---
+++ app/(home)/Trending.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+type Props = {}
+
+const Trending = (props: Props) => {
+  return (
+    <section className='pt-3 pb-10'>
+        <div className='flex items-center gap-3'>
+            <div className='bg-wh-900 py-2 text-wh-10 text-sm font-bold'>
+                TRENDING
+            </div>
+            <p>
+                
+            </p>
+        </div>
+    </section>
+  )
+}
+
+export default Trending;
