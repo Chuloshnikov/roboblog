87220e458eaa512c73600ef22f351a96f28c679e
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-24 17:46:11 +0300

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Tech from "./(home)/Tech";
 import Trending from "./(home)/Trending";
 
 
@@ -7,7 +8,7 @@ export default function Home() {
       <Trending/>
       <div className="md:flex gap-10 mb-5">
         <div className="basis-3/4">
-         {/*<Tech/>*/ } 
+         <Tech/>
           {/*<Travel/>*/ }
           {/*<Other/>*/ }
           <div className="hidden md:block">
