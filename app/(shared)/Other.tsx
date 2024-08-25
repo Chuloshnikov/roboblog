7baa6b9cdbd95731b5b3d3f2123422a47d482475
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-25 09:58:24 +0300

    done

---
+++ app/(shared)/Other.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Card from './Card';
+
+type Props = {}
+
+const Other = (props: Props) => {
+  return (
+    <section>
+        <hr className='border'/>
+        {/*HEADER*/}
+        <p className='font-bold text-2xl my-8'>Other Trending Posts</p>
+        <div className='sm:grid grid-cols-2 gap-16'>
+            <Card 
+            className='bg-wh-500 mt-5 sm:mt-0' 
+            imageHeight='h-80' 
+            />
+            <Card 
+            className='bg-wh-500 mt-5 sm:mt-0' 
+            imageHeight='h-80' 
+            />
+            <Card 
+            className='bg-wh-500 mt-5 sm:mt-0' 
+            imageHeight='h-80' 
+            />
+            <Card 
+            className='bg-wh-500 mt-5 sm:mt-0' 
+            imageHeight='h-80' 
+            />
+        </div>
+    </section>
+  )
+}
+
+export default Other;
